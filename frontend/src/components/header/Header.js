04fb516b4742024eb4aa16adc2160e41c366ed13
Niddam <louniddam@gmail.com>
2020-10-19 11:32:07 +0200

    dump style

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import '../header/Header.css'
 import { connect } from "react-redux";
 import {signoutAction} from "../../storeRedux/actions/signoutActions"
 class Header extends React.Component {
@@ -15,24 +16,24 @@ class Header extends React.Component {
     console.log(this.props.signinStore.userToken);
     if(this.props.signinStore.userToken == null){
         x = (<header className="header">
-          <nav className="navba">
-          <Link to="/">Home</Link>
-          <Link to="/signup">Signup</Link>
+          <nav className="nav">
+          <Link className="nav_link"  to="/">Home</Link>
+          <Link className="nav_link"  to="/signup">Signup</Link>
           </nav>
         </header>)
          
       }else if (this.props.signinStore.userToken){
          x =  (<header className="header">
-        <nav className="navbar">
-        <Link to="/">Home</Link>
+        <nav className="nav">
+        <Link className="nav_link"  to="/">Home</Link>
         
-        <Link to="/productform">Product Form</Link>
+        <Link className="nav_link"  to="/productform">Product Form</Link>
         {/* pensez a ajouter le panier */}
-        <button onClick={this.onSignout} >Sign out</button>
+        <button onClick={this.onSignout} className="signout_btn">Sign out</button>
         </nav>
       </header>)
     }
-    return(<div>{x}</div>)
+    return(<div className="main_container">{x}</div>)
    
   }
 }
